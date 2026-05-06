84b4464dfc8e7dd44f2c481b07051c652d51d69a
Majestic Monkey <brody.a.schmitt@gmail.com>
2026-05-05 20:29:49 -0400

    Initalizing Repository

---
+++ addons/beziersurfaces/Scripts/ControlPoint.cs
@@ -0,0 +1,73 @@
+using Godot;
+using Godot.Collections;
+using System;
+using static BezierSurfaceBuilder.BezierSurfaceBuilder;
+
+namespace BezierSurfaces
+{
+	[GlobalClass, Tool]
+	public partial class ControlPoint : MeshInstance3D
+	{
+		public Vector2 Loc = new Vector2(0, 0);
+
+		public bool HasSurface = false;
+
+		
+		public override void _EnterTree()
+		{
+			Loc = LocFromName();
+		}
+
+		public void CreateSurface()
+		{
+			if (!HasSurface)
+			{
+				HasSurface = true;
+				NotifyPropertyListChanged();
+				var forklift = GetParent();
+				if (forklift is BezierSurfaceBuilder.BezierSurfaceBuilder builder)
+				{
+					builder.CreateSurfaceExternally(Loc);
+				}
+				RotationDegrees = new Vector3(90, 0, 0);
+			}
+			else
+			{
+				NotifyPropertyListChanged();
+			}
+		}
+
+		public void RemoveSurface()
+		{
+			if (HasSurface)
+			{
+				HasSurface = false;
+				NotifyPropertyListChanged();
+				var forklift = GetParent();
+				if (forklift is BezierSurfaceBuilder.BezierSurfaceBuilder builder)
+				{
+					builder.RemoveSurfaceExternally(Loc);
+				}
+				RotationDegrees = new Vector3(0, 0, 0);
+			}
+			else
+			{
+				NotifyPropertyListChanged();
+			}
+		}
+
+		public Vector2 LocFromName()
+		{
+			Vector2 Location = new Vector2(0, 0);
+			string MyName = this.Name;
+			for (var i = 0; i < MyName.Length; i++)
+			{
+
+			}
+			string[] SplitName = MyName.Split("_");
+			Location.X = float.Parse(SplitName[1]);
+			Location.Y = float.Parse(SplitName[2]);
+			return Location;
+		}
+	}
+}
---
+++ addons/beziersurfaces/Scripts/control_point_inspector_features.gd
@@ -0,0 +1,30 @@
+@tool
+extends EditorInspectorPlugin
+
+
+func _can_handle(object: Object) -> bool:
+	return object is ControlPoint
+
+
+func _parse_begin(object: Object) -> void:
+	var Builder = object.get_parent()
+
+	var reload_button = Button.new()
+	reload_button.text = "Reload Surfaces"
+	reload_button.pressed.connect(Builder.UpdateSurfaces)
+	add_custom_control(reload_button)
+
+	var surface_button = Button.new()
+	if object.HasSurface:
+		surface_button.text = "Remove Surface Here"
+		surface_button.pressed.connect(object.RemoveSurface)
+		add_custom_control(surface_button)
+	else:
+		surface_button.text = "Add Surface Here"
+		surface_button.pressed.connect(object.CreateSurface)
+		add_custom_control(surface_button)
+
+func _parse_property(object: Object, type: Variant.Type, name: String, hint_type: PropertyHint, hint_string: String, usage_flags: int, wide: bool) -> bool:
+	if name == "position":
+		return false
+	return true
